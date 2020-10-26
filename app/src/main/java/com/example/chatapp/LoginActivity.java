4c5294c06bd967043db65064b9e44aa13dbc9380
daksh <dakshverma.verma@gmail.com>
2020-10-26 13:16:07 +0530

    updated login activity user interface and fixed some typos

--- app/src/main/java/com/example/chatapp/LoginActivity.java
+++ app/src/main/java/com/example/chatapp/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import android.text.TextUtils;
@@ -16,6 +17,7 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -38,19 +40,28 @@ public class LoginActivity extends AppCompatActivity {
     EditText email;
     EditText password;
     Button emailsignin;
-
+    TextView register;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mAuth=FirebaseAuth.getInstance();
 
         email=findViewById(R.id.editTextTextPersonName);
         password=findViewById(R.id.editTextTextPassword);
         emailsignin=findViewById(R.id.button2);
+        register=(TextView) findViewById(R.id.register_textview);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this,"Register User",Toast.LENGTH_SHORT).show();
+            }
+        });
 
         emailsignin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,7 +70,7 @@ public class LoginActivity extends AppCompatActivity {
                 String userpass=password.getText().toString();
                 if (TextUtils.isEmpty(userdata) | TextUtils.isEmpty(userpass))
                 {
-                    Toast.makeText(LoginActivity.this, "Fill all feilds", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Fill all fields", Toast.LENGTH_LONG).show();
                 }
                 else {
                     if( !Patterns.EMAIL_ADDRESS.matcher(userdata).matches()){
@@ -69,7 +80,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                     else if (password.length()<6)
                     {
-                        password.setError("Password Length should atleast be 6");
+                        password.setError("Password should be at least 6 characters long");
                         password.requestFocus();
                         return;
                     }
@@ -82,7 +93,7 @@ public class LoginActivity extends AppCompatActivity {
                                     Toast.makeText(LoginActivity.this,"User Signed In",Toast.LENGTH_SHORT).show();
                                 }
                                 else {
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.makeText(LoginActivity.this, "Authentication failed",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
